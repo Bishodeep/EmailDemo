45cdd5d8d1a05c7f18f3f65b8b0944438138ff23
Bishodeep <Bisowray1069@gmail.com>
2019-02-20 12:41:09 -0800

    first

---
+++ EmailDemo/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using EmailDemo.Models;
+
+namespace EmailDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress(model.ToEmail));  // replace with valid value 
+                message.To.Add("mistye999@gmail.com");  // replace with valid value 
+                message.To.Add("Kaushtup103@gmail.com");  // replace with valid value 
+                message.From = new MailAddress(model.FromEmail);  // replace with valid value
+                message.Subject = model.Subject;
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+                if (model.Upload != null && model.Upload.ContentLength > 0)
+                {
+                    message.Attachments.Add(new Attachment(model.Upload.InputStream, Path.GetFileName(model.Upload.FileName)));
+                }
+                try
+                {
+                    using (var smtp = new SmtpClient())
+                    {
+                        //var credential = new NetworkCredential
+                        //{
+                        //    UserName = "bisowray1069@gmail.com",  // replace with valid value
+                        //    Password = "*********"  // replace with valid value
+                        //};
+                        //smtp.Credentials = credential;
+                        //smtp.Host = "smtp.gmail.com";
+                        //smtp.Port = 587;
+                        //smtp.EnableSsl = true;
+                        await smtp.SendMailAsync(message);
+                        return RedirectToAction("Sent");
+                    }
+                }
+                catch (Exception e)
+                {
+                    return View();
+                    throw;
+                }
+               
+            }
+            return View(model);
+        }
+        public ActionResult Sent()
+        {
+            return View();
+        }
+    }
+}
---
+++ EmailDemo/Models/EmailFormModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmailDemo.Models
+{
+    public class EmailFormModel
+    {
+        [Required, Display(Name = "Your name")]
+        public string FromName { get; set; }
+        [Required, Display(Name = "Your email"), EmailAddress]
+        public string FromEmail { get; set; }
+        [Required, Display(Name = "To"), EmailAddress]
+        public string ToEmail { get; set; }
+        [Required]
+        public string Message { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        
+        public HttpPostedFileBase Upload { get; set; }
+    }
+}
